a8a1d28155636414eefef9eb827631a493c87a9b
Pschioeler <140426149+Pschioeler@users.noreply.github.com>
2023-12-08 10:28:08 +0100

    pushed from pbw

--- frontend/public/js/data.js
+++ frontend/public/js/data.js
@@ -1,32 +1,29 @@
-const GET_WORKSHOP_URL = "http://13.50.4.110:1337/workshops";
+const GET_WORKSHOP_URL = "http://hss.zteffano.dk:1337/workshopsjoined";
 
 var workshopList = [];
 
-async function fetchData() {
-    // fecthing data from API and save it in session storage
-workshopList = fetch(GET_WORKSHOP_URL)
-.then(response => {
-    if(!response.ok) {
-        throw new Error("Network response was not ok");
-    }
-    return response.json();
-})
-.then (jsonData => {
-    jsonData.forEach(element => {
-        workshopList.push(element);
+// fecthing data from API and save it in session storage
+fetch(GET_WORKSHOP_URL)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    })
+    .then (jsonData => {
+        jsonData.forEach(element => {
+            workshopList.push(element);
+        });
+        console.log('Data fetched and saved to workshoplist', workshopList);
+        sortArrayByDate(workshopList);
+        console.log('array sorted', workshopList);
+        displayAllWorkshops();
+            
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
+        console.error('There was a problem fetching the data:', error);
     });
-    console.log('Data fetched and saved to workshoplist', workshopList);
-    sortArrayByDate(workshopList);
-    console.log('array sorted', workshopList);
-    displayAllWorkshops();
-        
-})
-.catch(error => {
-    // Handle any errors that occurred during the fetch
-    console.error('There was a problem fetching the data:', error);
-});
-}
-
 
 // sort data by date
 function sortArrayByDate(arrayToSort) {
@@ -34,7 +31,7 @@ function sortArrayByDate(arrayToSort) {
         let workshopA = a.start;
         let workshopB = b.start;
         if (workshopA < workshopB) {
-            return -1;
+            return 1;
         }
         if (workshopA > workshopB) {
             return 1;
@@ -58,9 +55,10 @@ function sortArrayByDate(arrayToSort) {
     })
 }
 
+
 // display workshops
 function displayAllWorkshops() {
-    var workshopDisplay = document.getElementById("workshopDisplay");
+    var workshopDisplay = document.getElementById("courseOverviewCards");
 
     workshopList.forEach(element => {
         const workshopElement = createWorkshopElement(element);
@@ -74,20 +72,53 @@ function createWorkshopElement(workshop) {
     workshopElement.classList.add('workshop');
 
     const startDato = formatDato(workshop.start);
-    const statusTekst = workshop.status === 'completed' ? 'ikke plads' : 'stadig plads';
+    const statusTekst = workshop.status === 'completed' ? 'Kurset er desværre allerede fuldt booket' : 'stadig plads';
+
+    // get city by locationAddress
+    const locationAddress = workshop.locationAddress;
+    console.log(locationAddress)
+    const parts = locationAddress.split(' ');
+    const city = parts.pop();
+
+    // Limit the workshop description to 50 characters 
+    const truncatedDescription = workshop.description.length > 150 ?
+        workshop.description.slice(0, 150) + ' ...' :
+        workshop.description;
 
     workshopElement.innerHTML = `
-        <div class="workshop-status ${workshop.status}">${statusTekst}</div>
-        <img class="workshop-image" src="${workshop.logo}" alt="${workshop.name}">
-        <div class="workshop-content">
-            <p class="workshop-date"> ${startDato}</p>
-            <p class="workshop-adress"> ${workshop.adress}</p>
-            <p class="workshop-firma"> ${workshop.firma}</p>
-            <h2 class="workshop-title">${workshop.name}</h2>
-            <p class="workshop-discription">${workshop.description}</p>
-            <a class="workshop-a" href="${workshop.workshopLink}" target="_blank">Læs mere og tilmeld</a>
+        <div class="categoryBar"> ${workshop.categoryName}</div>
+        <div class="container">
+            <img class="workshopImage" src="${workshop.logo}" alt="${workshop.name}">
+            <h2 class="workshopTitle">${workshop.name}</h2>
+        </div>
+        <div class="workshopContent">
+            <p class="workshopDateAndLocation">${startDato}, ${city}</p>
+            <p class="workshopDiscription">${truncatedDescription}</p>
+            ${workshop.status !== 'completed' ? `<button class="button-link" onclick="redirectToPage('${workshop.workshopLink}')">Læs mere og tilmeld</button>` : `<p class="workshopStatus">${statusTekst}</p>`}
         </div>
     `;
+
+// assign color to categoryBar by category
+const categoryElement = workshopElement.querySelector(".categoryBar");
+    switch (workshop.categoryName) {
+        case "Tech":
+            categoryElement.classList.add("tech");
+            break;
+        // Add more cases for other categories as needed
+        case "Social":
+            categoryElement.classList.add("social");
+            break;
+        case "Business":
+            categoryElement.classList.add("business");
+            break;
+        case "Creative":
+            categoryElement.classList.add("creative");
+            break;
+        default:
+            categoryElement.classList.add("other");
+            break;
+    }
+
     return workshopElement;
 }
 
--- frontend/public/js/saveData.js
+++ frontend/public/js/saveData.js
@@ -3,20 +3,30 @@ import { fetchData } from "./fetchModule.js";
 const GET_WORKSHOP_URL = "http://hss.zteffano.dk:1337/workshopsjoined";
 
 // declaring arrays
+var workshopListe = [];
 var newWorkshops = [];
 var oldWorkshops = [];
 
-function fetchWorkshopData() {
+async function fetchWorkshopData() {
     try{
-        var workshopListe = fetchData(GET_WORKSHOP_URL)
+        var data = await fetchData(GET_WORKSHOP_URL)
         .then(response => {
-            console.log(response); // This will log the fetched data
-    
-            seperateWorkshops(response);
-    
-            console.log("New Workshops:", newWorkshops);
-            console.log("Old Workshops:", oldWorkshops);
+            if(!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
         })
+        .then (response => {
+            response.forEach(element => {
+                workshopList.push(element);
+            });
+            console.log('Data fetched and saved to workshoplist', workshopList);
+            sortArrayByDate(workshopList);
+            console.log('array sorted', workshopList);
+            displayAllWorkshops();
+                
+        })
+        
     }
     catch (error) {
     console.error("Error fetching or processing data:", error);
