d671136013577cf3d3ee7e2783150d269c3711a0
Pschioeler <140426149+Pschioeler@users.noreply.github.com>
2023-12-08 09:41:52 +0100

    Update saveData.js

--- frontend/public/js/saveData.js
+++ frontend/public/js/saveData.js
@@ -1,6 +1,6 @@
 import { fetchData } from "./fetchModule.js";
 
-const GET_WORKSHOP_URL = "http://13.50.4.110:1337/workshops";
+const GET_WORKSHOP_URL = "http://hss.zteffano.dk:1337/workshopsjoined";
 
 // declaring arrays
 var newWorkshops = [];
@@ -8,13 +8,15 @@ var oldWorkshops = [];
 
 function fetchWorkshopData() {
     try{
-        var workshopListe = fetchData(GET_WORKSHOP_URL);
-        console.log(workshopListe); // This will log the fetched data
-
-        seperateWorkshops(workshopListe);
-
-        console.log("New Workshops:", newWorkshops);
-        console.log("Old Workshops:", oldWorkshops);
+        var workshopListe = fetchData(GET_WORKSHOP_URL)
+        .then(response => {
+            console.log(response); // This will log the fetched data
+    
+            seperateWorkshops(response);
+    
+            console.log("New Workshops:", newWorkshops);
+            console.log("Old Workshops:", oldWorkshops);
+        })
     }
     catch (error) {
     console.error("Error fetching or processing data:", error);
