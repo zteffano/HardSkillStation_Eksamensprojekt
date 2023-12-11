20cf3b83e9fa51765606acf4efa70353a2aeafc9
zteffano <48279765+zteffano@users.noreply.github.com>
2023-12-11 07:22:58 +0100

    Update af Company endpoints til at medtage location, somn midlertidig løsning - senere ryger location helt som model. + ved delete af workshop, fjern den også alle entries i AccountWorkshop

--- backend/WEBAPI_SOLUTION/HSS_WEBAPI_MICROSERVICE/Endpoints/CompanyEndpoints.cs
+++ backend/WEBAPI_SOLUTION/HSS_WEBAPI_MICROSERVICE/Endpoints/CompanyEndpoints.cs
@@ -10,25 +10,54 @@ namespace HSS_WEBAPI_MICROSERVICE.Endpoints
     {
         public static void MapCompanyEndpoints(this WebApplication app)
         {
-            app.MapGet("/companies", async (HSSContext context, IMapper mapper) =>
-            {
-                List<Company> companies = await context.Companies.ToListAsync();
-                List<CompanyGetDTO> companyDTOs = mapper.Map<List<CompanyGetDTO>>(companies);
-
-                return Results.Ok(companyDTOs);
-            }).WithTags("Company Endpoints");
-
-			app.MapGet("/company/{id}", async (HSSContext context, IMapper mapper, int id) =>
+			app.MapGet("/companies", async (HSSContext context) =>
 			{
-				Company company = await context.Companies.FindAsync(id);
+				// Inkluderer Location data, når vi henter Companies. Midlertidig løsning. Senere merger vi dem og fjerner location helt
+				List<Company> companies = await context.Companies
+													   .Include(c => c.Location)
+													   .ToListAsync();
+
+				// Mapper Companies til CompanyGetDTO, inklusive de nye Location data
+				List<CompanyGetDTO> companyDTOs = companies.Select(c => new CompanyGetDTO
+				{
+					Id = c.Id,
+					Name = c.Name,
+					LocationId = c.LocationId,
+					Address = c.Location?.Address,
+					Website = c.Location?.Website,
+					ContactEmail = c.Location?.ContactEmail
+				}).ToList();
+
+				return Results.Ok(companyDTOs);
+			}).WithTags("Company Endpoints");
+
+
+			app.MapGet("/company/{id}", async (HSSContext context, int id) =>
+			{
+				// Inkluderer Location data, når du henter den specifikke Company
+				Company company = await context.Companies
+											   .Include(c => c.Location)
+											   .FirstOrDefaultAsync(c => c.Id == id);
+
 				if (company == null)
 				{
 					return Results.NotFound($"Company with ID {id} not found.");
 				}
 
-				CompanyGetDTO companyDTO = mapper.Map<CompanyGetDTO>(company);
+				// Mapper Company til CompanyGetDTO
+				CompanyGetDTO companyDTO = new CompanyGetDTO
+				{
+					Id = company.Id,
+					Name = company.Name,
+					LocationId = company.LocationId,
+					Address = company.Location?.Address,
+					Website = company.Location?.Website,
+					ContactEmail = company.Location?.ContactEmail
+				};
+
 				return Results.Ok(companyDTO);
 			}).WithTags("Company Endpoints");
+
 
 
 			app.MapPost("/createcompany", async (CompanyCreateDTO companyDTO, HSSContext context, IMapper mapper) =>
--- backend/WEBAPI_SOLUTION/HSS_WEBAPI_MICROSERVICE/Program.cs
+++ backend/WEBAPI_SOLUTION/HSS_WEBAPI_MICROSERVICE/Program.cs
@@ -10,8 +10,6 @@ using HSS_WEBAPI_MICROSERVICE.Endpoints;
 using HSS_WEBAPI_MICROSERVICE.DTO.AccountDTO;
 using HSS_WEBAPI_MICROSERVICE.DTO;
 
-// TODO
-
 
 namespace HSS_WEBAPI_MICROSERVICE
 {
@@ -26,31 +24,27 @@ namespace HSS_WEBAPI_MICROSERVICE
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 			
+			// Connection string logic - til at skjule credentials i connectionstring og til brug af miljøvariabler
 			string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
 			.Replace("(DB_SERVER)", Environment.GetEnvironmentVariable("DB_SERVER"))
 			.Replace("(DB_USER)", Environment.GetEnvironmentVariable("DB_USER"))
 			.Replace("(DB_PASSWORD)", Environment.GetEnvironmentVariable("DB_PASSWORD"))
 			.Replace("(DB_DATABASENAME)", Environment.GetEnvironmentVariable("DB_DATABASENAME"));
-			//Console.WriteLine("ConnSTRING" + connectionString);
 
 			//Skal kommenteres ud, hvis der ikke testes på lokal database, ellers overrider den vores connectionstring logic.
-			//connectionString = builder.Configuration.GetConnectionString("TestConnection");
+			connectionString = builder.Configuration.GetConnectionString("TestConnection");
 
 			builder.Services.AddDbContext<HSSContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 			// Auto mapper
 			builder.Services.AddAutoMapper(typeof(MappingProfile));
-			//Til brug af vores EventBrite Api 
-			//builder.Services.AddHttpClient();
-			//builder.Services.AddTransient<EventBriteApi>();
 			var app = builder.Build(); 
 
-			// Configure the HTTP request pipeline.
-			//if (app.Environment.IsDevelopment())
-			//{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			//}
+			// Vi tillader pt. Swagger at være aktiv i alle miljøer
+	
+			app.UseSwagger();
+			app.UseSwaggerUI();
+
 
 			app.UseHttpsRedirection();
 			app.UseAuthorization();
