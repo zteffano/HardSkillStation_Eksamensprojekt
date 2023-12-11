b195aa70b6b4e6a64cdb7d73241a5f7cb43d2a72
Philip Schiøler <philip@schioeler.org>
2023-12-11 09:09:32 +0100

    something

--- frontend/public/js/test.js
+++ frontend/public/js/test.js
@@ -27,25 +27,6 @@ creativeBtn.addEventListener('click', () => {
    
 });
 
-/* Lagt de gamle kald på knapperne i Headeren */
-
-/*
-const headerBtns = document.querySelectorAll(".buttons button");
-
-headerBtns[0].innerHTML = "Alle WS";
-headerBtns[1].innerHTML = "Nye WS";
-headerBtns[2].innerHTML = "Gamle WS";
-console.log(headerBtns);
-headerBtns[0].addEventListener('click', () => {
-    Display.displayAllWorkshops();
-});
-headerBtns[1].addEventListener('click', () => {
-    Display.displayNewWorkshops();
-});
-headerBtns[2].addEventListener('click', () => {
-    Display.displayOldWorkshops();
-});
-*/
 document.addEventListener('DOMContentLoaded', () => {
     Display.startUp(true); // Den vises nu i decending rækkefølge, hvis det skal være omvendt, kan der parses en true til metoden.
     Display.displayCompanies();
