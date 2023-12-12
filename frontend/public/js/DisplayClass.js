beb7bc406e40d5050a0ebe05bd08a359044f8a99
macand842 <55792391+macand842@users.noreply.github.com>
2023-12-12 21:25:00 +0100

    history page fix and aditional stuff

--- frontend/public/js/DisplayClass.js
+++ frontend/public/js/DisplayClass.js
@@ -99,7 +99,7 @@ export class Display
                 let element = document.getElementById("companyDisplay");
                 element.innerHTML = "";
                 data.forEach(company => {
-                    element.innerHTML += company.name + "<br>"
+                    element.innerHTML += `• ${company.name}<br>`
                 })
                 
                 
